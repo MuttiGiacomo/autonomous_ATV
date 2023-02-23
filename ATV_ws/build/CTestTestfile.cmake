cd1d2bd79fe0162d9a859c3ca5c94afd6a1e936d
MuttiGiacomo <giacomo.mutti@studenti.unitn.it>
2023-02-23 15:20:14 +0200

    added ws

---
+++ ATV_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/agtek/autonomous_ATV/ATV_ws/src
+# Build directory: /home/agtek/autonomous_ATV/ATV_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
