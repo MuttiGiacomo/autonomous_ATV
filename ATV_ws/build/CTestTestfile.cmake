fca5e78b13f5b1793b33d764a40a12ea22e3e6c1
MuttiGiacomo <giacomo.mutti@studenti.unitn.it>
2023-03-16 15:55:58 +0200

    organized enviroment, teleop from keyboard is ok

--- ATV_ws/build/CTestTestfile.cmake
+++ ATV_ws/build/CTestTestfile.cmake
@@ -6,3 +6,4 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
 subdirs("teleop_twist_keyboard")
+subdirs("teleop_twist_joy")
--- ATV_ws/build/cmake_install.cmake
+++ ATV_ws/build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/agtek/autonomous_ATV/ATV_ws/build/gtest/cmake_install.cmake")
   include("/home/agtek/autonomous_ATV/ATV_ws/build/teleop_twist_keyboard/cmake_install.cmake")
+  include("/home/agtek/autonomous_ATV/ATV_ws/build/teleop_twist_joy/cmake_install.cmake")
 
 endif()
 
