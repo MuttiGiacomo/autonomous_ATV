19a42633aaa2edb1c138a3522217704068c4bbbd
MuttiGiacomo <giacomo.mutti@studenti.unitn.it>
2023-05-24 10:39:09 +0300

    deleted build and devel

--- ATV_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/giacomo/autonomous_ATV/ATV_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/giacomo/autonomous_ATV/ATV_ws/devel;/opt/ros/melodic'.split(';') if '/home/giacomo/autonomous_ATV/ATV_ws/devel;/opt/ros/melodic' != '' else []
